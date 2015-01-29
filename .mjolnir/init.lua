e994fa0ea4cedc8b4be13de8463ac72887fb35ed
Miquel Adell <miquel.adell@rsbmedia.com>
2015-01-29 09:10:06 +0100

    szymonkaliski mjolnir

--- .mjolnir/init.lua
+++ .mjolnir/init.lua
@@ -1,155 +1,460 @@
--- Load Extensions
 local application = require "mjolnir.application"
-local window = require "mjolnir.window"
-local hotkey = require "mjolnir.hotkey"
-local keycodes = require "mjolnir.keycodes"
-local fnutils = require "mjolnir.fnutils"
-local alert = require "mjolnir.alert"
-local screen = require "mjolnir.screen"
--- User packages
-local grid = require "mjolnir.bg.grid"
-local hints = require "mjolnir.th.hints"
-local appfinder = require "mjolnir.cmsj.appfinder"
+local eventtap    = require "mjolnir._asm.eventtap"
+local fnutils     = require "mjolnir.fnutils"
+local hotkey      = require "mjolnir.hotkey"
+local keycodes    = require "mjolnir.keycodes"
+local timer       = require "mjolnir._asm.timer"
+local transform   = require "mjolnir.sk.transform"
+local window      = require "mjolnir.window"
 
-local definitions = nil
-local hyper = nil
+-- extensions
+local ext = {
+  frame = {},
+  win = {},
+  app = {},
+  utils = {}
+}
 
-local gridset = function(frame)
-  return function()
-    local win = window.focusedwindow()
-    if win then
-      grid.set(win, frame, win:screen())
-    else
-      alert.show("No focused window.")
+-- saved window positions
+ext.win.positions = {}
+
+-- window extension settings
+ext.win.margin     = 10
+ext.win.animate    = true
+ext.win.fixenabled = false
+ext.win.fullframe  = false
+
+-- check if simbl is running
+-- if so, then it's for menubarhider,
+-- and fullframe should be anabled
+if os.execute("ps xc | grep -q SIMBL") then
+  ext.win.fullframe = true
+end
+
+-- returns frame pushed to screen edge
+function ext.frame.push(screen, direction)
+  local frames = {
+    up = function()
+      return {
+        x = ext.win.margin + screen.x,
+        y = ext.win.margin + screen.y,
+        w = screen.w - ext.win.margin * 2,
+        h = screen.h / 2 - ext.win.margin
+      }
+    end,
+
+    down = function()
+      return {
+        x = ext.win.margin + screen.x,
+        y = ext.win.margin * 3 / 4 + screen.h / 2 + screen.y,
+        w = screen.w - ext.win.margin * 2,
+        h = screen.h / 2 - ext.win.margin * (2 - 1 / 4)
+      }
+    end,
+
+    left = function()
+      return {
+        x = ext.win.margin + screen.x,
+        y = ext.win.margin + screen.y,
+        w = screen.w / 2 - ext.win.margin * (2 - 1 / 4),
+        h = screen.h - ext.win.margin * (2 - 1 / 4)
+      }
+    end,
+
+    right = function()
+      return {
+        x = ext.win.margin / 2 + screen.w / 2 + screen.x,
+        y = ext.win.margin + screen.y,
+        w = screen.w / 2 - ext.win.margin * (2 - 1 / 4),
+        h = screen.h - ext.win.margin * (2 - 1 / 4)
+      }
+    end
+  }
+
+  return frames[direction]()
+end
+
+-- returns frame moved by ext.win.margin
+function ext.frame.nudge(frame, screen, direction)
+  local modifyframe = {
+    up = function(frame)
+      frame.y = math.max(screen.y + ext.win.margin, frame.y - ext.win.margin)
+      return frame
+    end,
+
+    down = function(frame)
+      frame.y = math.min(screen.y + screen.h - frame.h - ext.win.margin * 3 / 4, frame.y + ext.win.margin)
+      return frame
+    end,
+
+    left = function(frame)
+      frame.x = math.max(screen.x + ext.win.margin, frame.x - ext.win.margin)
+      return frame
+    end,
+
+    right = function(frame)
+      frame.x = math.min(screen.x + screen.w - frame.w - ext.win.margin, frame.x + ext.win.margin)
+      return frame
+    end
+  }
+
+  return modifyframe[direction](frame)
+end
+
+-- returns frame sent to screen edge
+function ext.frame.send(frame, screen, direction)
+  local modifyframe = {
+    up = function(frame)
+      frame.y = screen.y + ext.win.margin
+    end,
+
+    down = function(frame)
+      frame.y = screen.y + screen.h - frame.h - ext.win.margin * 3 / 4
+    end,
+
+    left = function(frame)
+      frame.x = screen.x + ext.win.margin
+    end,
+
+    right = function(frame)
+      frame.x = screen.x + screen.w - frame.w - ext.win.margin
+    end
+  }
+
+  modifyframe[direction](frame)
+  return frame
+end
+
+-- returns frame fited inside screen
+function ext.frame.fit(frame, screen)
+  frame.w = math.min(frame.w, screen.w - ext.win.margin * 2)
+  frame.h = math.min(frame.h, screen.h - ext.win.margin * (2 - 1 / 4))
+
+  return frame
+end
+
+-- returns frame centered inside screen
+function ext.frame.center(frame, screen)
+  frame.x = screen.w / 2 - frame.w / 2 + screen.x
+  frame.y = screen.h / 2 - frame.h / 2 + screen.y
+
+  return frame
+end
+
+-- get screen frame
+function ext.win.screenframe(win)
+  local funcname = ext.win.fullframe and "fullframe" or "frame"
+  local winscreen = win:screen()
+  return winscreen[funcname](winscreen)
+end
+
+-- set frame
+function ext.win.set(win, frame, time)
+  time = time or 0.15
+
+  if ext.win.animate then
+    transform:setframe(win, frame, time)
+  else
+    win:setframe(frame)
+  end
+end
+
+-- ugly fix for problem with window height when it's as big as screen
+function ext.win.fix(win)
+  if ext.win.fixenabled then
+    local screen = ext.win.screenframe(win)
+    local frame = win:frame()
+
+    if (frame.h > (screen.h - ext.win.margin * (2 - 1 / 4))) then
+      frame.h = screen.h - ext.win.margin * 10
+      ext.win.set(win, frame)
     end
   end
 end
 
-auxWin = nil
-function saveFocus()
-  auxWin = window.focusedwindow()
-  alert.show("Window '" .. auxWin:title() .. "' saved.")
+-- pushes window in direction
+function ext.win.push(win, direction)
+  local screen = ext.win.screenframe(win)
+  local frame
+
+  frame = ext.frame.push(screen, direction)
+
+  ext.win.fix(win)
+  ext.win.set(win, frame)
 end
-function focusSaved()
-  if auxWin then
-    auxWin:focus()
+
+-- nudges window in direction
+function ext.win.nudge(win, direction)
+  local screen = ext.win.screenframe(win)
+  local frame = win:frame()
+
+  frame = ext.frame.nudge(frame, screen, direction)
+  ext.win.set(win, frame, 0.05)
+end
+
+-- push and nudge window in direction
+function ext.win.pushandnudge(win, direction)
+  ext.win.push(win, direction)
+  ext.win.nudge(win, direction)
+end
+
+-- sends window in direction
+function ext.win.send(win, direction)
+  local screen = ext.win.screenframe(win)
+  local frame = win:frame()
+
+  frame = ext.frame.send(frame, screen, direction)
+
+  ext.win.fix(win)
+  ext.win.set(win, frame)
+end
+
+-- centers window
+function ext.win.center(win)
+  local screen = ext.win.screenframe(win)
+  local frame = win:frame()
+
+  frame = ext.frame.center(frame, screen)
+  ext.win.set(win, frame)
+end
+
+-- fullscreen window with margin
+function ext.win.full(win)
+  local screen = ext.win.screenframe(win)
+  local frame = {
+    x = ext.win.margin + screen.x,
+    y = ext.win.margin + screen.y,
+    w = screen.w - ext.win.margin * 2,
+    h = screen.h - ext.win.margin * (2 - 1 / 4)
+  }
+
+  ext.win.fix(win)
+  ext.win.set(win, frame)
+
+  -- center after setting frame, fixes terminal
+  ext.win.center(win)
+end
+
+-- throw to next screen, center and fit
+function ext.win.throw(win, direction)
+  local framefunc = ext.win.fullframe and "fullframe" or "frame"
+  local screenfunc = direction == "next" and "next" or "previous"
+
+  local winscreen = win:screen()
+  local throwscreen = winscreen[screenfunc](winscreen)
+  local screen = throwscreen[framefunc](throwscreen)
+
+  local frame = win:frame()
+
+  frame.x = screen.x
+  frame.y = screen.y
+
+  frame = ext.frame.fit(frame, screen)
+  frame = ext.frame.center(frame, screen)
+
+  ext.win.fix(win)
+  ext.win.set(win, frame)
+
+  win:focus()
+
+  -- center after setting frame, fixes terminal and macvim
+  ext.win.center(win)
+end
+
+-- set window size and center
+function ext.win.size(win, size)
+  local screen = ext.win.screenframe(win)
+  local frame = win:frame()
+
+  frame.w = size.w
+  frame.h = size.h
+
+  frame = ext.frame.fit(frame, screen)
+  frame = ext.frame.center(frame, screen)
+
+  ext.win.set(win, frame)
+end
+
+-- save and restore window positions
+function ext.win.pos(win, option)
+  local id = win:application():bundleid()
+  local frame = win:frame()
+
+  -- saves window position if not saved before
+  if option == "save" and not ext.win.positions[id] then
+    ext.win.positions[id] = frame
+  end
+
+  -- force update saved window position
+  if option == "update" then
+    ext.win.positions[id] = frame
+  end
+
+  -- restores window position
+  if option == "load" and ext.win.positions[id] then
+    ext.win.set(win, ext.win.positions[id])
   end
 end
 
-local hotkeys = {}
+-- cycle application windows
+-- https://github.com/nifoc/dotfiles/blob/master/mjolnir/cycle.lua
+function ext.win.cycle(win)
+  local windows = win:application():allwindows()
+  windows = fnutils.filter(windows, function(win) return win:isstandard() end)
 
-function createHotkeys()
-  for key, fun in pairs(definitions) do
-    local mod = hyper
-    if string.len(key) == 2 and string.sub(key,2,2) == "c" then
-      mod = {"cmd"}
+  if #windows >= 2 then
+    table.sort(windows, function(a, b) return a:id() < b:id() end)
+    local activewindowindex = fnutils.indexof(windows, win)
+
+    if activewindowindex then
+      activewindowindex = activewindowindex + 1
+      if activewindowindex > #windows then activewindowindex = 1 end
+
+      windows[activewindowindex]:focus()
     end
-    
-    local hk = hotkey.new(mod, string.sub(key,1,1), fun)
-    table.insert(hotkeys, hk)
-    hk:enable()
   end
 end
 
-function rebindHotkeys()
-  for i, hk in ipairs(hotkeys) do
-    hk:disable()
-  end
-  hotkeys = {}
-  createHotkeys()
-  alert.show("Rebound Hotkeys")
-end
+-- launch or focus or cycle app
+function ext.app.launchorfocus(app)
+  local focusedwindow = window.focusedwindow()
+  local currentapp = focusedwindow and focusedwindow:application():title() or nil
 
-function applyPlace(win, place)
-  local scrs = screen:allscreens()
-  local scr = scrs[place[1]]
-  grid.set(win, place[2], scr)
-end
+  if currentapp == app then
+    if focusedwindow then
+      local appwindows = focusedwindow:application():allwindows()
+      local visiblewindows = fnutils.filter(appwindows, function(win) return win:isstandard() end)
 
-function applyLayout(layout)
-  return function()
-    for appName, place in pairs(layout) do
-      local app = appfinder.app_from_name(appName)
-      if app then
-        for i, win in ipairs(app:allwindows()) do
-          applyPlace(win, place)
-        end
+      if #visiblewindows == 0 then
+        -- try sending cmd-n for new window if no windows are visible
+        ext.utils.newkeyevent({ cmd = true }, "n", true):post()
+        ext.utils.newkeyevent({ cmd = true }, "n", false):post()
+      else
+        -- cycle windows if there are any
+        ext.win.cycle(focusedwindow)
       end
     end
+  else
+    application.launchorfocus(app)
   end
 end
 
-function init()
-  createHotkeys()
-  keycodes.inputsourcechanged(rebindHotkeys)
-  alert.show("Mjolnir, at your service.")
+-- smart browser launch or focus or cycle
+function ext.app.browser()
+  local browsers = { "Safari", "Google Chrome" }
+
+  local runningapps = application.runningapplications()
+  local focusedwindow = window.focusedwindow()
+  local currentapp = focusedwindow and focusedwindow:application():title() or nil
+
+  -- filter running applications by browsers array
+  local runningbrowsers = fnutils.map(browsers, function(browser)
+    return fnutils.find(runningapps, function(app) return app:title() == browser end)
+  end)
+
+  -- try to get index of current app in running browsers
+  -- this means - is one of the browsers currently selected
+  local currentindex = fnutils.indexof(fnutils.map(runningbrowsers, function(app)
+    return app:title()
+  end), currentapp)
+
+  -- if there are no browsers launch the first (default) one
+  -- otherwise cycle between browser windows or between browsers depending on situation
+  if #runningbrowsers == 0 then
+    ext.app.launchorfocus(browsers[1])
+  else
+    local browserindex = currentindex and (currentindex % #runningbrowsers) + 1 or 1
+    ext.app.launchorfocus(runningbrowsers[browserindex]:title())
+  end
 end
 
--- Actual config =================================
+-- properly working newkeyevent
+-- https://github.com/nathyong/mjolnir.ny.tiling/blob/master/spaces.lua
+function ext.utils.newkeyevent(modifiers, key, pressed)
+  local keyevent
 
-hyper = {"cmd", "shift", "ctrl"}
--- Set grid size.
-grid.GRIDWIDTH  = 6
-grid.GRIDHEIGHT = 8
-grid.MARGINX = 0
-grid.MARGINY = 0
-local gw = grid.GRIDWIDTH
-local gh = grid.GRIDHEIGHT
+  keyevent = eventtap.event.newkeyevent({}, "", pressed)
+  keyevent:setkeycode(keycodes.map[key])
+  keyevent:setflags(modifiers)
 
+  return keyevent
+end
 
-local goleft = {x = 0, y = 0, w = gw/2, h = gh}
-local goright = {x = gw/2, y = 0, w = gw/2, h = gh}
-local gobig = {x = 0, y = 0, w = gw, h = gh}
-local gomiddle = {x = 1, y = 1, w = 4, h = 6}
+-- apply function to a window with optional params, saving it's position for restore
+function dowin(fn, param)
+  local win = window.focusedwindow()
 
-local quarter1 = {x = 0,    y = 0,    w = gw/2, h = gh/2}
-local quarter2 = {x = gw/2, y = 0,    w = gw/2, h = gh/2}
-local quarter3 = {x = 0,    y = gh/2, w = gw/2, h = gh/2}
-local quarter4 = {x = 0,    y = gh/2, w = gw/2, h = gh/2}
+  if win and not win:isfullscreen() then
+    ext.win.pos(win, "save")
+    fn(win, param)
+  end
+end
 
-local fullApps = {
-  "Safari","Aurora","Nightly","Xcode","Qt Creator","Google Chrome",
-  "Google Chrome Canary", "Eclipse", "Coda 2", "iTunes", "Emacs", "Firefox"
-}
-local layout2 = {
-  Mail = {1, gomiddle},
-  Spotify = {1, gomiddle},
-  Calendar = {1, gomiddle},
-  Dash = {1, gomiddle},
-}
-fnutils.each(fullApps, function(app) layout2[app] = {1, gobig} end)
+-- for simple hotkey binding
+function bindwin(fn, param)
+  return function() dowin(fn, param) end
+end
 
-definitions = {
-  i = saveFocus,
-  o = focusSaved,
-  
-  x = gridset(gomiddle),
-  a = gridset(goleft),
-  s = grid.maximize_window,
-  d = gridset(goright),
-  
-  u = gridset(quarter1),
-  i = gridset(quarter2),
-  o = gridset(quarter3),
-  p = gridset(quarter4),
-  
-  l = applyLayout(layout2),
-  
-  z = grid.pushwindow_nextscreen,
-  r = mjolnir.reload,
-  q = function() appfinder.app_from_name("Mjolnir"):kill() end,
+-- apply function to a window with a timer
+function timewin(fn, param)
+  return timer.new(0.05, function() dowin(fn, param) end)
+end
 
-  v = function() hints.appHints(window.focusedwindow():application()) end,
-  c = hints.windowHints
-}
+-- keyboard modifier for bindings
+local mod1 = { "cmd", "ctrl" }
+local mod2 = { "cmd", "alt" }
+local mod3 = { "cmd", "alt", "ctrl" }
+local mod4 = { "cmd", "alt", "shift" }
+
+-- basic bindings
+hotkey.bind(mod1, "c", bindwin(ext.win.center))
+hotkey.bind(mod1, "z", bindwin(ext.win.full))
+hotkey.bind(mod1, "s", bindwin(ext.win.pos, "update"))
+hotkey.bind(mod1, "r", bindwin(ext.win.pos, "load"))
+
+-- cycle throught windows of the same app
+hotkey.bind(mod1, "tab", function() ext.win.cycle(window.focusedwindow()) end)
+
+-- move window to different screen
+hotkey.bind(mod4, "right", bindwin(ext.win.throw, "prev"))
+hotkey.bind(mod4, "left",  bindwin(ext.win.throw, "next"))
+
+-- push to edges and nudge
+fnutils.each({ "up", "down", "left", "right" }, function(direction)
+  local nudge = timewin(ext.win.nudge, direction)
+
+  hotkey.bind(mod1, direction, bindwin(ext.win.pushandnudge, direction))
+  hotkey.bind(mod2, direction, bindwin(ext.win.send, direction))
+  hotkey.bind(mod3, direction, function() nudge:start() end, function() nudge:stop() end)
+end)
+
+-- set window sizes
+fnutils.each({
+  { key = "1", w = 1400, h = 940 },
+  { key = "2", w = 980,  h = 920 },
+  { key = "3", w = 800,  h = 880 },
+  { key = "4", w = 800,  h = 740 },
+  { key = "5", w = 700,  h = 740 },
+  { key = "6", w = 850,  h = 620 },
+  { key = "7", w = 770,  h = 470 }
+}, function(object)
+  hotkey.bind(mod1, object.key, bindwin(ext.win.size, { w = object.w, h = object.h }))
+end)
 
 -- launch and focus applications
 fnutils.each({
-  { key = "e", app = "Google Chrome" },
-  { key = "u", app = "Atom" },
-  { key = "i", app = "Terminal" }
-  }, function(object)
-    definitions[object.key] = function() application.launchorfocus(object.app) end
-    end)
-    
-    init()
+  { key = "c", app = "Calendar" },
+  { key = "f", app = "Finder" },
+  { key = "n", app = "Notational Velocity" },
+  { key = "p", app = "TaskPaper" },
+  { key = "r", app = "Reminders" },
+  { key = "t", app = "Terminal" }
+}, function(object)
+  hotkey.bind(mod3, object.key, function() ext.app.launchorfocus(object.app) end)
+end)
+
+-- launch or focus browser in a smart way
+hotkey.bind(mod3, "b", function() ext.app.browser() end)
+
+-- reload mjolnir
+hotkey.bind(mod3, "m", function() mjolnir.reload() end)
